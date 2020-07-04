1cbea9a7a91137a21d64ef53557bb7f262da1461
Golden_Chow <570384231@qq.com>
2020-07-04 11:08:41 +0800

    LeetCode题目二：两数相加。把答案和题目抽离

--- Algorithm/src/main/java/algorithm/two/Answer.java
+++ Algorithm/src/main/java/algorithm/two/Answer.java
@@ -8,6 +8,32 @@ package algorithm.two;
  */
 public class Answer {
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
+        int carry = 0;
 
+        while (l1 != null || l2 != null) {
+
+            int num1 = l1 == null ? 0 : l1.val;
+            int num2 = l2 == null ? 0 : l2.val;
+
+            // 获取进位和当前位
+            int sum = num1 + num2 + carry;
+            carry = sum / 10;
+            sum %= 10;
+            cur.next = new ListNode(sum);
+
+            // 下一个节点值
+            cur = cur.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+
+            if (carry == 1) {
+                cur.next = new ListNode(carry);
+            }
+        }
+        return pre.next;
+    }
 
 }
---
+++ Algorithm/src/main/java/algorithm/two/ListNode.java
@@ -0,0 +1,15 @@
+package algorithm.two;
+
+/**
+ * @author: Kam-Chou
+ * @date: 2020/7/4 11:07
+ * @description: ListNode节点
+ * @version: 1.0
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+
+}
